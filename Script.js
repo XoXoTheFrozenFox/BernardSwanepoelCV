0844636a0ab0f272f5e9a80fd8c4c2be25eceecd
Bernard Swanepoel <104361159+XoXoTheFrozenFox@users.noreply.github.com>
2023-11-09 13:05:42 +0200

    Added
    Added

--- Script.js
+++ Script.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const imgBx = document.querySelector('.imgBx');
+    const modal = document.getElementById('customModal');
 
     imgBx.addEventListener('mouseover', () => {
         applyGrayscale();
     });
 
     imgBx.addEventListener('mouseout', () => {
-        removeGrayscale();
+        if (!modal.style.display || modal.style.display === 'none') {
+            removeGrayscale();
+        }
     });
 
     imgBx.addEventListener('touchstart', (event) => {
@@ -15,7 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     imgBx.addEventListener('touchend', () => {
-        removeGrayscale();
+        if (!modal.style.display || modal.style.display === 'none') {
+            removeGrayscale();
+        }
     });
 
     function applyGrayscale() {
@@ -25,4 +30,19 @@ document.addEventListener('DOMContentLoaded', function () {
     function removeGrayscale() {
         imgBx.style.filter = 'grayscale(0%)';
     }
+
+    // Modal-related functions
+    function showCustomModal() {
+        modal.style.display = 'block';
+        applyGrayscale(); // Apply grayscale when modal is displayed
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+        removeGrayscale(); // Remove grayscale when modal is closed
+    }
+
+    // Optionally, you can expose these functions to be used globally if needed
+    window.showCustomModal = showCustomModal;
+    window.closeModal = closeModal;
 });
